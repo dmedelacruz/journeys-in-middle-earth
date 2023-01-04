50a546d66ac5ba7d298bddc0971022b70fd45f6d
dmedelacruz <dmedelacruz10@gmail.com>
2023-01-04 22:15:26 +0800

    init

---
+++ src/main/java/com/dmdc/jime/JourneysInMiddleEathApplication.java
@@ -0,0 +1,13 @@
+package com.dmdc.jime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourneysInMiddleEathApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JourneysInMiddleEathApplication.class, args);
+    }
+
+}
